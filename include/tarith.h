ac7376c57e41a484b84862e587291bfe48f085bb
mollycooddle <toolya.telnoff@yandex.ru>
2024-12-06 02:03:18 +0300

    base

--- include/tarith.h
+++ include/tarith.h
@@ -1,3 +1,162 @@
 ﻿#pragma once
 
 #include <iostream>
+#include <string>
+#include <stack>
+#include <vector>
+
+class Calculator{
+	std::vector<std::string> terms;
+	std::stack<std::string> stack;
+public:
+	Calculator(std::string str = "")
+	{
+		std::string tmp;
+		for (int i = 0; i < str.size(); i++) {
+			tmp += str[i];
+			terms.push_back(tmp);
+			tmp.clear();
+		}
+
+		std::vector<std::string> numbers = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+		std::vector<std::string> operations = { "+", "-", "*", "/"};
+
+		int i = 0;
+		while(i < size() - 1)
+		{
+
+			while ((terms[i + 1] != "+") && (terms[i + 1] != "-") && (terms[i + 1] != "*") && (terms[i + 1] != "/") && \
+				   (terms[i] != "+") && (terms[i] != "-") && (terms[i] != "*") && (terms[i] != "/")) {
+				for (std::string j : numbers) {
+					if (i + 1 >= size()) {
+						break;
+					}
+
+					if (terms[i + 1] == j) {
+						terms[i] += terms[i + 1];
+						terms.erase(terms.begin() + i + 1);
+					}
+				}
+
+				if (i + 1 >= size()) {
+					break;
+				}
+			}
+			
+			if (i + 1 >= size()) {
+				break;
+			}
+
+			for (std::string oper : operations) {
+				if (terms[i] == oper) {
+					continue;
+				}
+			}
+
+			i++;
+		}
+	}
+
+	void stackFilling() {
+		for (int i = 0; i < size(); i++) {
+			if ((terms[i] == "*") || (terms[i] == "/")) {
+				stack.push(terms[i]);
+				terms.erase(terms.begin() + i);
+				
+			}
+			if ((terms[i] == "+") || (terms[i] == "-")) {
+				if (!stack.empty())
+					if (stack.top() == "*" || stack.top() == "/") {
+						calculation();
+					}
+
+				stack.push(terms[i]);
+				terms.erase(terms.begin() + i);
+			}
+		}
+	}
+
+	void print_stack() {
+		std::cout << stack.top() << std::endl;
+	}
+
+	double calculation() {
+		stackFilling();
+
+		while (size() != 1) {
+			if (stack.empty())
+				break;
+
+			if (stack.top() == "*") {
+				terms[size() - 2] = std::to_string(std::stod(terms[size() - 1]) * std::stod(terms[size() - 2]));
+				
+				stack.pop();
+				terms.pop_back();
+			}
+
+			if (stack.empty())
+				break;
+
+			if (stack.top() == "/") {
+				terms[size() - 2] = std::to_string(std::stod(terms[size() - 2]) / std::stod(terms[size() - 1]));
+
+				stack.pop();
+				terms.pop_back();
+			}
+
+			if (stack.empty())
+				break;
+
+			if (stack.top() == "+") {
+				terms[size() - 2] = std::to_string(std::stod(terms[size() - 1]) + std::stod(terms[size() - 2]));
+
+				stack.pop();
+				terms.pop_back();
+			}
+
+			if (stack.empty())
+				break;
+
+			if (stack.top() == "-") {
+				terms[size() - 2] = std::to_string(std::stod(terms[size() - 2]) - std::stod(terms[size() - 1]));
+
+				stack.pop();
+				terms.pop_back();
+			}
+
+			if (stack.empty())
+				break;
+
+		}
+
+		return std::stod(terms[0]);
+	}
+
+	std::string& operator[](size_t ind)
+	{
+		return terms[ind];
+	}
+	const std::string& operator[](size_t ind) const
+	{
+		return terms[ind];
+	}
+
+	void erase(int index) {
+		terms.erase(terms.begin() + index);
+	}
+
+	int size() 
+	{
+		return terms.size();
+	}
+
+	friend std::ostream& operator<<(std::ostream& ostr, const Calculator& other)
+	{
+		
+		for (int i = 0; i < other.terms.size(); i++) {
+			std::cout << other.terms[i] << std::endl;
+		}
+
+		return ostr;
+	}
+};
--- test/test_tarith.cpp
+++ test/test_tarith.cpp
@@ -1,8 +1,69 @@
-#include "tmatrix.h"
+#include "tarith.h"
 
 #include <gtest.h>
 
-TEST(TDynamicMatrix, can_create_matrix_with_positive_length)
+TEST(Calculator, test_numbers) 
 {
-  ASSERT_NO_THROW(TDynamicMatrix<int> m(5));
+
+	Calculator yr("235");
+
+	ASSERT_NO_THROW(std::cout << yr);
+}
+
+TEST(Calculator, test_numbers_not_sort)
+{
+
+	Calculator yr("230");
+
+	ASSERT_NO_THROW(std::cout << yr);
+}
+
+TEST(Calculator, test_numbers_with_operation)
+{
+
+	Calculator yr("230+2");
+
+	ASSERT_NO_THROW(std::cout << yr);
+}
+
+TEST(Calculator, test_numbers_with_operation_different_numbers)
+{
+
+	Calculator yr("230+993");
+
+	ASSERT_NO_THROW(std::cout << yr);
+}
+
+TEST(Calculator, test_numbers_with_operation_few_actions)
+{
+
+	Calculator yr("230+993*12");
+
+	ASSERT_NO_THROW(std::cout << yr);
+}
+
+TEST(Stack, test_stack_filling)
+{
+
+	Calculator yr("230+993*12");
+	yr.stackFilling();
+	std::cout << yr << std::endl;
+
+	ASSERT_NO_THROW(yr.print_stack());
+}
+
+TEST(calculator, test_culculation)
+{
+
+	Calculator yr("230+993*12");
+
+	EXPECT_EQ(12146, yr.calculation());
+}
+
+TEST(calculator, test_many_operations)
+{
+
+	Calculator yr("2-56*2+4/2+45/9+5-2*6");
+
+	EXPECT_EQ(-110, yr.calculation());
 }
